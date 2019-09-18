0c5ca781b7bb2c796cfb07c6f2fa00d79cb6432a
kenystev <kjestevez.0795@gmail.com>
2019-09-17 20:05:00 -0600

    [FIX] close button style - background

--- src/components/presentationals/CloseButton.tsx
+++ src/components/presentationals/CloseButton.tsx
@@ -5,9 +5,12 @@ const CloseButton = styled(Box)`
 	position: absolute;
 	top: 1em;
 	right: 1.5em;
+    padding: .2em .4em;
 	font-size: ${props => props.theme.fontSizes[3]}px;
 	font-weight: ${props => props.theme.fontWeights.medium};
 	color: ${props => props.theme.colors.red};
+	background-color: ${props => props.theme.colors.white};;
+    border-radius: 8px;
 	cursor: pointer;
 `;
 
